84daa845b8d67adfdc59079fd8373dd3d1b28f54
zakarya khelifa <nigrazaki@gmail.com>
2026-09-21 03:31:59 +0400

    Refine booking state handling in booking_service_screen.dart

--- lib/screens/customer/booking_service_screen.dart
+++ lib/screens/customer/booking_service_screen.dart
@@ -45,7 +45,6 @@ class _BookingServiceScreenState extends ConsumerState<BookingServiceScreen> {
         serviceDuration: selected.duration,
         serviceDurationMinutes: selected.durationMinutes,
         selectedServices: [selected],
-        resetStaffSelection: true,
         resetAppointmentSelection: true,
       );
       context.push('/booking-specialist');
