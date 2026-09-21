1d3d024ca8ae41b3efc98286f1d6585dbb7e4060
zakarya khelifa <nigrazaki@gmail.com>
2026-09-22 00:58:24 +0400

    Map trusted minimum lead-time errors to customer UI

--- lib/services/booking_functions_service.dart
+++ lib/services/booking_functions_service.dart
@@ -310,11 +310,14 @@ class BookingFunctionsService {
     }
     if (msg.contains('INVALID_CANONICAL_ALIGNMENT') ||
         msg.contains('START_TIME_IN_PAST') ||
+        msg.contains('START_TIME_TOO_SOON') ||
         msg.contains('INVALID_BOOKING_INTERVAL')) {
       return InvalidBookingTimeException(
-        msg.contains('START_TIME_IN_PAST')
-            ? 'Please select a future appointment time.'
-            : 'Appointment start time must be aligned to 15-minute intervals.',
+        msg.contains('START_TIME_TOO_SOON')
+            ? 'Please select an appointment at least 30 minutes from now.'
+            : msg.contains('START_TIME_IN_PAST')
+                ? 'Please select a future appointment time.'
+                : 'Appointment start time must be aligned to 15-minute intervals.',
       );
     }
 
